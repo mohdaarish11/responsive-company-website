b1addb4e2e467f7eb07b52780d336bef6a866d04
Mohd Aarish <muhammadaarish628@gmail.com>
2026-06-25 11:43:37 +0530

    Initial commit

---
+++ text.js
@@ -0,0 +1,19 @@
+const menuBtn = document.getElementById("menu-btn");
+
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    alert(
+        "Thank you! Your message has been submitted."
+    );
+
+    form.reset();
+});
